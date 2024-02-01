e9a1bfbc78a29169523f3659ba38a2b5ad9255ee
covertimpala <77851547+covertimpala@users.noreply.github.com>
2024-02-01 16:57:16 +0100

    Update movementsimplified.py

--- movementsimplified.py
+++ movementsimplified.py
@@ -45,28 +45,34 @@ def calculateab(locx, locy, o, r1, r2, r3, _range, bypass, x_dist, y_dist):
                     if nocolor == 0:
                         print(Fore.RED + "Additional", o) #Uncertain has been updated to "Additional"
                         print(Fore.RESET + "",at,bt,c, "or", cp)
-                        return [at, bt, c]
+                        return [at, bt, c, "a"]
                     else:
                         print("Additional", o)
                         print(at,bt,cp)
-                        return [at, bt, c]
+                        return [at, bt, c, "a"]
+                else:
+                    print(Fore.RED + "angle out of range" + Fore.RESET)
+                    return ("out of range")
             elif c >= _range[0] and c <=_range[1]:
                 if nocolor == 0:
                     print(Fore.GREEN + "Point on circle (angle degrees):", o, "location:", locx, locy)
                     print(Fore.RESET + "Degrees:",at,bt,c)
-                    return [at, bt, c]
+                    return [at, bt, c, ""]
                 else:
                     print("Point on circle (angle degrees):", o, "location:", locx, locy)
                     print("Degrees:",at,bt,c)
-                    return [at, bt, c]
+                    return [at, bt, c, ""]
+        else:
+            print(Fore.RED + "angle out of range" + Fore.RESET)
+            return ("out of range")
     else:
         print(Fore.RED + "angle out of range" + Fore.RESET)
         #print()
         return ("out of range")
 
-def choosepos(segment1, segment2, segment3, x_dist, y_dist, step, _range, bypass):
-    x_p = x_dist+offstx
-    y_p = y_dist+offsty
+def choosepos(segment1, segment2, segment3, x_dist, y_dist, step, _range, bypass, offstx_, offsty_):
+    x_p = x_dist+offstx_
+    y_p = y_dist+offsty_
     joint3loc = [x_p, y_p]
     try:
         return(calculateab(joint3loc[0], joint3loc[1],"", segment1, segment2, segment3, _range, bypass, x_dist, y_dist))
@@ -77,7 +83,7 @@ def choosepos(segment1, segment2, segment3, x_dist, y_dist, step, _range, bypass
 
 an_a = math.radians(30)
 an_b = math.radians(50)
-an_c = math.radians(50)
+an_c = math.radians(10)
 destination = [5,5]
 currpos = [r1*math.sin(an_a)+r2*math.sin(an_a+an_b)+r3*math.sin(an_a+an_b+an_c),r1*math.cos(an_a)+r2*math.cos(an_a+an_b)+r3*math.cos(an_a+an_b+an_c)]
 steps = 200
@@ -87,7 +93,7 @@ offstx = -r3*math.sin(an_a+an_b+an_c) #currpos[0]-r3*math.sin(an_c)
 offsty = -r3*math.cos(an_a+an_b+an_c) #currpos[1]-r3*math.cos(an_c)
 print(offstx, offsty)
 
-abcang = choosepos(r1, r2, r3, destination[0], destination[1], step, _range, bypass)
+abcang = choosepos(r1, r2, r3, destination[0], destination[1], step, _range, bypass, offstx, offsty)
 print(abcang)
 if abcang != "out of range":
     dan_a = math.degrees(an_a)
@@ -102,3 +108,50 @@ if abcang != "out of range":
         dan_b = dan_b + b_mstp
         dan_c = dan_c + c_mstp
         print(dan_a, dan_b, dan_c)
+else:
+    print(Fore.CYAN + "Check for closest match? (y/n)")
+    print("(this will shift the locked segment)")
+    an_s = input()
+    if an_s == "y" or an_s == "yes":
+        print(Fore.GREEN + "finding closest match" + Fore.RESET)
+        thetax = round(math.degrees(math.asin(offstx/r3)),5)
+        thetay = round(math.degrees(math.acos(offsty/r3)),5)
+        print(thetax, thetay)
+        if abs(thetax) == abs(thetay):
+            sol = abs(thetax)
+            global va
+            for i in range(89):
+                sol1 = math.radians((sol*i)/89 + sol)
+                sol2 = math.radians((sol*i)/89 - sol)
+                x_pp1 = r3*math.cos(math.radians(sol1))#r3*math.cos(math.radians(i))
+                y_pp1 = r3*math.sin(math.radians(sol1))#r3*math.sin(math.radians(i))
+                print(sol1, sol2)
+                print(x_pp1, y_pp1)
+                va = choosepos(r1, r2, r3, destination[0], destination[1], step, _range, bypass, x_pp1, y_pp1)
+                #print(va)
+                if va != "out of range":
+                    break
+                x_pp1 = r3*math.cos(math.radians(sol2))#r3*math.cos(math.radians(i))
+                y_pp1 = r3*math.sin(math.radians(sol2))#r3*math.sin(math.radians(i))
+                print(x_pp1, y_pp1)
+                va = choosepos(r1, r2, r3, destination[0], destination[1], step, _range, bypass, x_pp1, y_pp1)
+                #print(va)
+                if va != "out of range":
+                    break
+            if va != "out of range":
+                dan_a = math.degrees(an_a)
+                dan_b = math.degrees(an_b)
+                dan_c = math.degrees(an_c)
+                a_mstp = (va[0] - dan_a)/steps
+                b_mstp = (va[1] - dan_b)/steps
+                c_mstp = (va[2] - dan_c)/steps
+                for i in range(steps-1):
+                    #if i != 0:
+                    dan_a = dan_a + a_mstp
+                    dan_b = dan_b + b_mstp
+                    dan_c = dan_c + c_mstp
+                    print(dan_a, dan_b, dan_c)
+            else:
+                print(Fore.RED + "no solution" + Fore.RESET)
+        else:
+            print(Fore.RED + "Something went wrong" + Fore.RESET)
