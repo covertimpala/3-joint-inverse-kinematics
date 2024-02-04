0014654f63f25628c2f0e9f4b238361192f6d340
covertimpala <77851547+covertimpala@users.noreply.github.com>
2024-02-04 16:52:23 +0100

    Update movementsimplified.py

--- movementsimplified.py
+++ movementsimplified.py
@@ -12,25 +12,25 @@ x_dist = 3.3
 y_dist = 6.2
 step = 2 #359*step|| used to determine the number of points to check
 bypass = 1 #switch to  a value >= 4 to bypass filter
-_range = [-90, 90] #angle ranges (anything outside the range is filtered out (as long as bypass = 1))
+_range = [-90, 90, -90, 90, -90, 90] #angle ranges (anything outside the range is filtered out (as long as bypass = 1)) (a,a,b,b,c,c)
 #Degorrad = "deg" # choose between "deg" or "rad" (degrees or radians for output)
 
-def loc_of_J3():
-    print(r3*math.cos(an_c)/r3, r3*math.sin(an_c)/r3)
-    x_x = math.acos(-math.sin(an_c))
-    x_y = math.asin(math.cos(an_c))
-    print(x_x, ":", r1*math.cos(x_x), x_y)
-    if x_x == x_y:
-        return(x_x)
-    else:
-        return("no solution")
+#def loc_of_J3():
+ #   print(r3*math.cos(an_c)/r3, r3*math.sin(an_c)/r3)
+  #  x_x = math.acos(-math.sin(an_c))
+   # x_y = math.asin(math.cos(an_c))
+    #print(x_x, ":", r1*math.cos(x_x), x_y)
+    #if x_x == x_y:
+     #   return(x_x)
+    #else:
+    #    return("no solution")
 
 
 def calculateab(locx, locy, o, r1, r2, r3, _range, bypass, x_dist, y_dist):
     b = math.acos((locx**2 + locy**2 - r1**2 - r2**2) / (r1**2+r2**2))#(r1**2+r2**2)            (2*r1*r2)
     #print("b:", b)
     bt = math.degrees(b)
-    if bt >= _range[0]*bypass and bt <=_range[1]*bypass:
+    if bt >= _range[2]*bypass and bt <=_range[3]*bypass:
         a = math.atan(locx/locy)-math.atan((r2*math.sin(b))/(r1+(r2*math.cos(b))))
         #print("a:", a)
         at = math.degrees(a)
@@ -41,7 +41,7 @@ def calculateab(locx, locy, o, r1, r2, r3, _range, bypass, x_dist, y_dist):
             c = math.degrees(math.acos((ds**2+r3**2-ds3**2)/(2*ds*r3))) #Cosine rule
             if x_dist < locx:
                 cp = c-180
-                if cp >= _range[0]*bypass and cp <= _range[1]*bypass:
+                if cp >= _range[4]*bypass and cp <= _range[5]*bypass:
                     if nocolor == 0:
                         print(Fore.RED + "Additional", o) #Uncertain has been updated to "Additional"
                         print(Fore.RESET + "",at,bt,c, "or", cp)
@@ -53,7 +53,7 @@ def calculateab(locx, locy, o, r1, r2, r3, _range, bypass, x_dist, y_dist):
                 else:
                     print(Fore.RED + "angle out of range" + Fore.RESET)
                     return ("out of range")
-            elif c >= _range[0] and c <=_range[1]:
+            elif c >= _range[4] and c <=_range[5]:
                 if nocolor == 0:
                     print(Fore.GREEN + "Point on circle (angle degrees):", o, "location:", locx, locy)
                     print(Fore.RESET + "Degrees:",at,bt,c)
@@ -70,9 +70,13 @@ def calculateab(locx, locy, o, r1, r2, r3, _range, bypass, x_dist, y_dist):
         #print()
         return ("out of range")
 
-def choosepos(segment1, segment2, segment3, x_dist, y_dist, step, _range, bypass, offstx_, offsty_):
-    x_p = x_dist+offstx_
-    y_p = y_dist+offsty_
+def choosepos(segment1, segment2, segment3, x_dist, y_dist, step, _range, bypass, offstx_, offsty_, simpl, z):
+    if simpl == False:
+        x_p = x_dist+offstx_
+        y_p = y_dist+offsty_
+    else:
+        x_p = r3*math.cos(math.radians(z/step))#r3*math.cos(math.radians(i))
+        y_p = r3*math.sin(math.radians(z/step))#r3*math.sin(math.radians(i))
     joint3loc = [x_p, y_p]
     try:
         return(calculateab(joint3loc[0], joint3loc[1],"", segment1, segment2, segment3, _range, bypass, x_dist, y_dist))
@@ -81,9 +85,9 @@ def choosepos(segment1, segment2, segment3, x_dist, y_dist, step, _range, bypass
         #print(Fore.RED + "OUT OF RANGE", i/step, joint3loc[0], joint3loc[1])
         #continue
 
-an_a = math.radians(30)
+an_a = math.radians(50)
 an_b = math.radians(50)
-an_c = math.radians(10)
+an_c = math.radians(23)
 destination = [5,5]
 currpos = [r1*math.sin(an_a)+r2*math.sin(an_a+an_b)+r3*math.sin(an_a+an_b+an_c),r1*math.cos(an_a)+r2*math.cos(an_a+an_b)+r3*math.cos(an_a+an_b+an_c)]
 steps = 200
@@ -93,7 +97,7 @@ offstx = -r3*math.sin(an_a+an_b+an_c) #currpos[0]-r3*math.sin(an_c)
 offsty = -r3*math.cos(an_a+an_b+an_c) #currpos[1]-r3*math.cos(an_c)
 print(offstx, offsty)
 
-abcang = choosepos(r1, r2, r3, destination[0], destination[1], step, _range, bypass, offstx, offsty)
+abcang = choosepos(r1, r2, r3, destination[0], destination[1], step, _range, bypass, offstx, offsty, False, 1)
 print(abcang)
 if abcang != "out of range":
     dan_a = math.degrees(an_a)
@@ -118,39 +122,22 @@ else:
         thetay = round(math.degrees(math.acos(offsty/r3)),5)
         print(thetax, thetay)
         if abs(thetax) == abs(thetay):
-            sol = abs(thetax)
-            global va
-            for i in range(89):
-                sol1 = math.radians((sol*i)/89 + sol)
-                sol2 = math.radians((sol*i)/89 - sol)
-                x_pp1 = r3*math.cos(math.radians(sol1))#r3*math.cos(math.radians(i))
-                y_pp1 = r3*math.sin(math.radians(sol1))#r3*math.sin(math.radians(i))
-                print(sol1, sol2)
-                print(x_pp1, y_pp1)
-                va = choosepos(r1, r2, r3, destination[0], destination[1], step, _range, bypass, x_pp1, y_pp1)
-                #print(va)
+            for z in range(359*step):
+                va = choosepos(r1, r2, r3, destination[0], destination[1], step, _range, bypass, offstx, offsty, True, z)
+                print(va)
                 if va != "out of range":
-                    break
-                x_pp1 = r3*math.cos(math.radians(sol2))#r3*math.cos(math.radians(i))
-                y_pp1 = r3*math.sin(math.radians(sol2))#r3*math.sin(math.radians(i))
-                print(x_pp1, y_pp1)
-                va = choosepos(r1, r2, r3, destination[0], destination[1], step, _range, bypass, x_pp1, y_pp1)
-                #print(va)
-                if va != "out of range":
-                    break
-            if va != "out of range":
-                dan_a = math.degrees(an_a)
-                dan_b = math.degrees(an_b)
-                dan_c = math.degrees(an_c)
-                a_mstp = (va[0] - dan_a)/steps
-                b_mstp = (va[1] - dan_b)/steps
-                c_mstp = (va[2] - dan_c)/steps
-                for i in range(steps-1):
-                    #if i != 0:
-                    dan_a = dan_a + a_mstp
-                    dan_b = dan_b + b_mstp
-                    dan_c = dan_c + c_mstp
-                    print(dan_a, dan_b, dan_c)
+                    dan_a = math.degrees(an_a)
+                    dan_b = math.degrees(an_b)
+                    dan_c = math.degrees(an_c)
+                    a_mstp = (va[0] - dan_a)/steps
+                    b_mstp = (va[1] - dan_b)/steps
+                    c_mstp = (va[2] - dan_c)/steps
+                    for i in range(steps-1):
+                        #if i != 0:
+                        dan_a = dan_a + a_mstp
+                        dan_b = dan_b + b_mstp
+                        dan_c = dan_c + c_mstp
+                        print(dan_a, dan_b, dan_c)
             else:
                 print(Fore.RED + "no solution" + Fore.RESET)
         else:
